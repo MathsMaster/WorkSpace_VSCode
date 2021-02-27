639824c1507e7720781df3d22b18492c3a9b191a
xulp <1074275493@qq.com>
2021-02-27 10:59:41 +0800

    VsCode进行测试

--- C/CGrammar/Pointer/Demo.c
+++ C/CGrammar/Pointer/Demo.c
@@ -121,6 +121,7 @@ void testTwoWeiArrayPointer()
 {
     int arr[3][4] = { {0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11} };
     int p = arr;
+    
     printf("这里的p : %#X \n",p);
 }
 
@@ -165,7 +166,7 @@ int main()
     printf("\n\n9.15二维数组的指针（指向二维数组的指针）\n");
     testTwoWeiArrayPointer();
 
-    
+
 
     return 0;
 }
